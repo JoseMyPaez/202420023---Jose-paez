50bcec0a3e1736b5b4ce3b70ce81a7ebe28ff664
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-08-26 15:27:46 +0000

    2

--- HelloWord/helloWorld3.cpp
+++ HelloWord/helloWorld3.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
     string userName;
-int userAge;
-cout <<"What's you name:"
+    int userAge;
+cout <<"What's you name:";
 cin >> userName;
-cout << "How old are you:"
-cin >> userAge
+cout << "How old are you:";
+cin >> userAge ;
 cout<< "Hola "<< userName<<" usted ha ingresado que su edad es de "<< userAge<< "años." << endl ; 
 
 }
