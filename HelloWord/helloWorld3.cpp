a3dd2031a8a45effae544c26a41c49b676a48e74
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-08-26 15:26:27 +0000

    entrada edad y nombre

---
+++ HelloWord/helloWorld3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string userName;
+int userAge;
+cout <<"What's you name:"
+cin >> userName;
+cout << "How old are you:"
+cin >> userAge
+cout<< "Hola "<< userName<<" usted ha ingresado que su edad es de "<< userAge<< "años." << endl ; 
+
+}
