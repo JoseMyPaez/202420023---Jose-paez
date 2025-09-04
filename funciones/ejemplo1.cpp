b089732acd9115ff94489528794db42430584a9c
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-09-04 16:08:36 +0000

    ejemplo 2

--- funciones/ejemplo1.cpp
+++ funciones/ejemplo1.cpp
@@ -1,1 +1,8 @@
-include<iostream>
+#include<iostream>
+
+using namespace std;
+
+void numberIsLessThan0(int value){
+    cout<<"El numero es menor que o"<<endl;
+}
+
---
+++ funciones/ejemplo2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+int sumTwoValues(int valueOne, int valueTwo){
+    int sumResult= valueOne + valueTwo; 
+    cout<<"La suma de sus valores es de"<<sumResult<<endl;
+    return sumResult;
+}
+
+int subsTwoValues(int valueOne, int valueTwo){
+    int subResult= valueOne - valueTwo; 
+    cout<<"La suma de sus valores es de"<<subResult<<endl;
+    return subResult;
+}
+
+int multTwoValues(int valueOne, int valueTwo){
+    int multResult= valueOne + valueTwo; 
+    cout<<"La suma de sus valores es de"<<multResult<<endl;
+    return multResult;
+}
+
+void divTwoValues(int valueOne, int valueTwo){
+    if (valueTwo == 0)
+    {
+        cout<<"la division es indeterminada"<<endl;
+    }
+    else
+    {
+        int divResult= valueOne + valueTwo; 
+        cout<<"La suma de sus valores es de"<<divResult<<endl;
+    }
+   
+}
+
+void compareTwoValues(int valueOne,int valueTwo)
+{
+    if (valueOne>>valueTwo)
+    {
+        cout<<valueOne<<" es mayor que "<< valueTwo<<endl;
+    }
+    else
+    {
+        cout<<valueTwo<<" es mayor que "<< valueOne<<endl;
+    }
+    
+}
+
+void moduleTwoValues(int valueOne, int valueTwo)
+{
+    int moduleResult= valueOne % valueTwo; 
+    cout<<"La suma de sus valores es de"<<moduleResult<<endl;    
+}
+
+
+int main()
+{
+    int valueOne , valueTwo, operationValue, result;
+    cout<<"Ingrese su primer valor"<<endl;
+    cin>>valueOne;
+    cout<<"Ingrese su segundo valor"<<endl;
+    cin>>valueTwo;
+    cout<<"Que operacioin desea realizar: "<<endl<<"1   Suma"<<endl<<"2    Resta"<<endl<<"3    Multiplicacion"<<endl<<"4    Division"<<endl;
+    cin>>operationValue;
+    switch (operationValue)
+    {
+    case 1:
+        result=sumTwoValues(valueOne,valueTwo);
+        cout<<"El resultado de la suma es "<<result<<endl;
+        break;
+    case 2:
+        result=subsTwoValues(valueOne,valueTwo);
+        cout<<"El resultado de la resta es "<<result<<endl;
+        break;
+    case 3:
+        result=multTwoValues(valueOne, valueTwo);
+        cout<<"El resultado de la multiplicacion es "<<result<<endl;
+        break;
+    case 4: 
+        divTwoValues(valueOne,valueTwo);
+        break;
+    case 5:
+        moduleTwoValues(valueOne,valueTwo);
+        break;
+    default:
+        cout<<"La operacion que selecciono es inexistente"<<endl;
+        break;
+    }
+    return 0;
+}
