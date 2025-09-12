c064a61394edac8a2eaafabb7abc883150d49fd3
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-09-12 15:27:38 +0000

    weather

---
+++ funciones/temperatura.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+void valorateTemp(int temperature )
+{
+    if (temperature<15)
+    {
+        cout <<"Esta haciendo frio"<<endl;
+    }
+    else if (temperature>25)
+    {
+        cout <<"Esta haciendo calor"<<endl;
+    }
+    else
+    {
+        cout <<"El clima esta templado"<<endl;
+    }
+    
+}
+
+int main()
+{
+    int temperature;
+    cout<<"Ingrese la temperarura"<<endl;
+    cin>>temperature;
+    valorateTemp(temperature);
+    
+}
