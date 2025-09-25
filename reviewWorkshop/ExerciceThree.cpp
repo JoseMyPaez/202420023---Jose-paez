a488ba7bcf1592f738a44bb50440b06636192053
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-09-25 15:18:04 +0000

    modification 3

--- reviewWorkshop/ExerciceThree.cpp
+++ reviewWorkshop/ExerciceThree.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void printMultiplicationTableRange(int table, int start, int end){
+    for(int i = start; i <= end; i++){
+        cout<<table<<" x "<<i<<" = "<<table * i<<endl;
+    }
+
+}
+
 void printMultiplicationTable(int number){
     for(int i = 1; i <= 10; i++){
         cout<<number<<" x "<<i<<" = "<<number * i<<endl;
@@ -9,10 +16,16 @@ void printMultiplicationTable(int number){
 }
 
 int main(){
-    int table;
+    int table, start, end;
     cout<<"Dame el numero de una tabla de multiplicar: "<<endl;
     cin>>table;
     printMultiplicationTable(table);
+    cout<<"Dame el numero de inicio: "<<endl;
+    cin>>start;
+    cout<<"Dame el numero de fin: "<<endl;
+    cin>>end;
+    printMultiplicationTableRange(table, start, end);
+
     return 0;
 
 }
